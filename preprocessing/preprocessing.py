fe6c4124446d07d3cee7a5f3dd9a34683832261e
JinDepot <hyejinlee@bdai.snu.ac.kr>
2023-03-07 19:22:15 +0900

    set-up cuda on giga01

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -1,3 +1,22 @@
-import numpy as np
-import pandas as pd
-import os
+#import numpy as np
+#import pandas as pd
+#import os
+from sentence_transformers import SentenceTransformer
+import pickle
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+sentences = ['This framework generates embeddings for each input sentence', 'Sentences are passed as a list of string.', 'The quick brown fox jumps over the lazy dog.']
+
+embeddings = model.encode(sentences)
+
+#Store sentences & embeddings on disc
+with open('embeddings.pkl', "wb") as fOut:
+    pickle.dump({'sentences': sentences, 'embeddings': embeddings}, fOut, protocol=pickle.HIGHEST_PROTOCOL)
+
+#Load sentences & embeddings from disc
+with open('embeddings.pkl', "rb") as fIn:
+    stored_data = pickle.load(fIn)
+    stored_sentences = stored_data['sentences']
+    stored_embeddings = stored_data['embeddings']
+
+    
