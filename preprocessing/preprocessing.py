65b7de20530c7511bcc6df760b52cc42488bda4a
JinDepot <hyejinlee@bdai.snu.ac.kr>
2023-03-08 15:52:55 +0900

    edit preprocessing

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -1,22 +1,40 @@
-#import numpy as np
-#import pandas as pd
-#import os
-from sentence_transformers import SentenceTransformer
-import pickle
+# This file loads and preprocesses the business description sections, saving the resulting table as a csv file
+# Author: Hye Jin Lee
+# Last Updated: 03-08-2023
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
-sentences = ['This framework generates embeddings for each input sentence', 'Sentences are passed as a list of string.', 'The quick brown fox jumps over the lazy dog.']
+import pandas as pd
+import glob
+import os
+import re
 
 embeddings = model.encode(sentences)
+def main():
+    # load data
+    print('Loading data...')
+    data_path = '/home/hyejin/data/'
+    filelist = glob.glob(f'{data_path}/10-K/*', recursive=True)
+    len(filelist)
 
-#Store sentences & embeddings on disc
-with open('embeddings.pkl', "wb") as fOut:
-    pickle.dump({'sentences': sentences, 'embeddings': embeddings}, fOut, protocol=pickle.HIGHEST_PROTOCOL)
+    #Load sentences & embeddings from disc
+    for i, f in enumerate(filelist):
+        print("   working on %s-th document..." %i, end='\r')
+        # get ticker from filepath
+        ticker = f.split('/')[-1].split('_')[0]
 
-#Load sentences & embeddings from disc
-with open('embeddings.pkl', "rb") as fIn:
-    stored_data = pickle.load(fIn)
-    stored_sentences = stored_data['sentences']
-    stored_embeddings = stored_data['embeddings']
+        with open(f, 'r') as content:
+            content = content.read().replace('\r\n',' ').replace('\n',' ')
+            content = re.sub(r'^\W*','',content)
+            content = content.replace('Table of Contents',' ').replace('Overview', '').replace('OVERVIEW', '').replace('and Description of Major Subsidiaries ','')
+            content = re.sub(r'^G(?:\s*eneral\s*)\b\s+','',content)
+            content = ' '.join(content.split())
+            content = content.strip()
 
-    
+        df.loc[i,'ticker'] = ticker
+        df.loc[i,'content'] = content
+
+    # dump the resulting data
+    df.to_csv(f'{data_path}/sp500_all.csv', index=False)
+    print('Done!')
+
+if __name__ == '__main__':
+    main()
